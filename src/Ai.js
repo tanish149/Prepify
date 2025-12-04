53a83a78db180a72b374478c1dbc573e129231da
tanish149 <tanishsk7@gmail.com>
2025-12-04 13:57:09 +0530

    Update Ai.js

--- src/Ai.js
+++ src/Ai.js
@@ -4,21 +4,6 @@ const SYSTEM_PROMPT = `
 You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page
 `
 
-// 🚨👉 ALERT: Read message below! You've been warned! 👈🚨
-// If you're following along on your local machine instead of
-// here on Scrimba, make sure you don't commit your API keys
-// to any repositories and don't deploy your project anywhere
-// live online. Otherwise, anyone could inspect your source
-// and find your API keys/tokens. If you want to deploy
-// this project, you'll need to create a backend of some kind,
-// either your own or using some serverless architecture where
-// your API calls can be made. Doing so will keep your
-// API keys private.
-
-
-
-// Make sure you set an environment variable in Scrimba 
-// for HF_ACCESS_TOKEN
 const hf = new  InferenceClient(import.meta.env.VITE_API_KEY)
 
 export async function getRecipeFromMistral(ingredientsArr) {
@@ -37,3 +22,4 @@ export async function getRecipeFromMistral(ingredientsArr) {
         console.error(err.message)
     }
 }
+
